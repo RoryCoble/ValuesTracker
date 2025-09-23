c2ae3f7efd37780a2bce857c090306990bb58e31
RoryCoble <33813906+RoryCoble@users.noreply.github.com>
2025-09-22 20:45:20 -0500

    started adding cypress tests

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,9 +1,24 @@
 describe('Register page', () => {
   before(() => {
-      cy.task('userDb', 'TRUNCATE TABLE public.users')
+    cy.task('userDb', 'TRUNCATE TABLE public.users')
   })
+
+  beforeEach(() => {
+    cy.visit('/')
+    cy.contains('Register').dblclick()
+  })    
     
   it('displays the expected elements', () => {
-    cy.visit('/register')
+    cy.get('h1').should('be.visible').and('have.text', 'Register')
+    cy.get('[name="userName"]').should('be.visible').and('have.attr', 'placeholder', 'User Name')
+    cy.get('[name="password"]').should('be.visible').and('have.attr', 'placeholder', 'Password')  
+    cy.get('[name="email"]').should('be.visible').and('have.attr', 'placeholder', 'Email')
+    cy.get('[type="submit"]').should('be.visible').and('have.text', 'Submit')
+    cy.contains('Cancel').should('be.visible')  
   })
+
+  it('returns to the login page when cancel is clicked', () => {
+    cy.contains('Cancel').click()
+    cy.url().should('include', '/login')
+  })    
 })
