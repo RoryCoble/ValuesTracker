42e2ed2ba5549105c029549790912370e191ead3
RoryCoble <33813906+RoryCoble@users.noreply.github.com>
2025-09-28 12:01:28 -0500

    added more to cypress main tests

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,3 +1,9 @@
+const dayjs = require('dayjs')
+var utc = require("dayjs/plugin/utc");
+var timezone = require("dayjs/plugin/timezone");
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
 describe('Main page', () => {
 
   before(() => {
@@ -34,27 +40,30 @@ describe('Main page', () => {
           entityCode: 'AAAAA'  
         },
     })
-    for (let i = 1; i <= 10; i++) {
-        cy.request({
-            method: 'POST',
-            url: 'http://localhost:5002/dataseed/add_value',
-            form: true,
-            body: {
-              count: i,
-              entityCode: 'AAAAA'  
-            },
-        })
-        cy.wait(500)
-    } 
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:5002/dataseed/add_value',
+        form: true,
+        body: {
+          count: 1,
+          entityCode: 'AAAAA'  
+        },
+    }).as('data_point')
   })
   
   beforeEach(() => {
       cy.login('a','a')
   })
 
-  it('should display the expected elements', () => {
+  it('should display the expected elements', function () {
     cy.get('[data-testid="pageTitle"]').should('be.visible').and('have.text', 'Entity Tracker')
     cy.get('[data-testid="AAAAA-Header"]').should('be.visible').and('have.text', 'AAAAA')
-    cy.get('[class="recharts-responsive-container"]').should('be.visible')  
+    cy.get('[class="recharts-responsive-container"]').should('be.visible')
+    cy.get('[class="recharts-responsive-container"]').click()
+    cy.get('[class="recharts-tooltip-label"]').should('be.visible')
+        .and('contain.text', dayjs().tz('GMT').format('ddd, D MMM YYYY H:mm:')).and('contain.text','GMT')
+    cy.get('[class="recharts-tooltip-item"]').should('be.visible').and('have.text','value : ' + this.data_point.body)
+    cy.get('tspan').should('be.visible')
+        .and('contain.text', dayjs().tz('GMT').format('ddd, D MMM YYYY H:mm:')).and('contain.text','GMT')
   })
 })
